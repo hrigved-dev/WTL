341beb15d9e41caa3958325a84063973a74f87c8
hrigved-dev <hrigved.khatavkar@gmail.com>
2022-05-17 10:27:35 +0530

    update

--- phpprograms/electricityBill.php
+++ phpprograms/electricityBill.php
@@ -16,6 +16,35 @@
     if(isset($_POST['submit'])) {
         $units = $_POST['units'];
         
+        function calculateBill($u) {
+            $total = 0;
+            if($u > 250) {
+                $total = $u * 6.50;
+            }
+            else {
+                if($u <= 50) {
+                    $total = $u * 3.50;
+                }
+                elseif($u <= 100) {
+                    $total = 50 * 3.50;
+                    $u = $u - 50;
+                    $total = $total + ($u  * 4.00);
+                }
+                else {
+                    $total = 50 * 3.50;
+                    $u = $u - 50;
+                    $total = $total + (100 * 4.00);
+                    $u = $u - 100;
+                    $total = $total + ($u * 5.20);
+                }
+            }
+
+            return $total;
+        }
+
+        $result = calculateBill($units);
+
+        echo "The total bill for $units is dollars $result";
         
     }
 ?>
